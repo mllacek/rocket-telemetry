f10eb80e16b53a25c0989ffbfa0b233e68e409da
Monica Lacek <mll79@zips.uakron.edu>
2019-02-14 15:10:32 -0500

    Adding UART1

--- UART/UART.c
+++ UART/UART.c
@@ -29,12 +29,37 @@ void InitU2(void) {
     //RTS = 1; // default status , not ready to send
 }
 
+void InitU1(void) {
+    U1BRG = 415; // PIC24FJ128GA010 data sheet, 17.1 for calculation, Fcy = 16MHz.
+    //16MHz/9600/4-1 = 415
+    U1MODE = 0x8008; // See data sheet, pg 148. Enable UART1, BRGH = 1,
+    // Idle state = 1, 8 data, No parity, 1 Stop bit
+    U1STA = 0x0400; // See data sheet, pg. 150, Transmit Enable
+    // Following lines pertain Hardware handshaking
+    //TRISFbits.TRISF13 = 1; // enable RTS , output
+    //RTS = 1; // default status , not ready to send
+}
+char putU1(char c) { // (Transmit)
+    //while (CTS); //wait for !CTS (active low)
+    while (U1STAbits.UTXBF); // Wait if transmit buffer full.
+    U1TXREG = c; // Write value to transmit FIFO
+    return c;
+}
+
 char putU2(char c) { // (Transmit)
     //while (CTS); //wait for !CTS (active low)
     while (U2STAbits.UTXBF); // Wait if transmit buffer full.
     U2TXREG = c; // Write value to transmit FIFO
     return c;
 }
+
+char getU1 ( void ) //(Receive)
+{
+//RTS = 0; // telling the other side !RTS
+while (! U1STAbits . URXDA ); // wait
+//RTS =1; // telling the other side RTS
+return U1RXREG ; // from receiving buffer
+} //getU2
 
 char getU2 ( void ) //(Receive)
 {
@@ -44,13 +69,20 @@ while (! U2STAbits . URXDA ); // wait
 return U2RXREG ; // from receiving buffer
 } //getU2
 
-void UART_send_string(char* st_pt)
+void U2_send_string(char* st_pt)
 {
     while(*st_pt) //if there is a char
         putU2(*st_pt++);//process it as a byte data
 }
 
+char* pt U1_get_string(void)
+{
+    return *getU1();
+    //TODO
+}
+
 void main(void) {
+    InitU1();
     InitU2();
     ms_delay(500);
     srand(time(NULL));   // Initialization, should only be called once.
@@ -62,17 +94,20 @@ void main(void) {
         int r = rand()% 20; // Returns a pseudo-random integer between 0 and 19.
         t = t + r;
         sprintf (buffer, "%d", t);
-        UART_send_string(buffer);
+        U2_send_string(buffer);
         ms_delay(2); // wait 500 us as suggested
         putU2('\n');
         ms_delay(2); // wait 500 us as suggested
         r = rand()% 20;
         t = t - r;
         sprintf (buffer, "%d", t);
-        UART_send_string(buffer);
+        U2_send_string(buffer);
         ms_delay(2); // wait 500 us as suggested
         putU2('\n');
         ms_delay(500);
+        
+        getU1();
+        getU1();
     }
     
 }
