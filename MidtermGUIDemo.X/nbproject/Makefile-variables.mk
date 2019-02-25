74c191dad0908bf07a90e57945f620f54204e5d6
Monica Lacek <mll79@zips.uakron.edu>
2019-02-25 16:19:48 -0500

    Midterm GUI demo

---
+++ MidtermGUIDemo.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MidtermGUIDemo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MidtermGUIDemo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=midtermguidemo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/midtermguidemo.x.tar
