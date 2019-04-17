0010d452dcf8333259a468c5e46f1b84fcb31084
Monica Lacek <mll79@zips.uakron.edu>
2019-04-17 16:58:10 -0400

    Adding timing info

--- SensorIntegration.X/main.c
+++ SensorIntegration.X/main.c
@@ -8,8 +8,42 @@
 #include <libpic30.h>
 #include <stdio.h>
 
+int mSec = 0, Sec = 0, Min = 0; // Global variables
+
+void _ISR _T1Interrupt(void) {
+
+    mSec++; // increment the thousands of a second counter
+    if (mSec > 999) {
+        mSec = 0;
+        Sec++; // increment the seconds counter
+        if (Sec > 59) // 60 seconds make a minute
+        {
+            Sec = 0;
+            Min++;
+            if (Min > 180){
+                Min = 0;
+            }
+        }
+    } //seconds
+    _T1IF = 0;
+} // T1Interrupt
+
+void TimerSetup(){
+    _T1IP = 4; // this is the default value anyway
+    TMR1 = 0; // clear the timer
+    PR1 = 16000 - 1; // set the period register
+    T1CON = 0x8000; // enabled, prescaler 1:64, internal clock
+    _T1IF = 0; // clear the interrupt flag, before
+    _T1IE = 1; // enable the T1 interrupt source
+}
+
+void printTime(){
+    printf("%d:%d.%d,", Min,Sec, mSec);
+}
+
 int main(void) {
    
+    TimerSetup();
     InitU2();
     InitU1();
     SPI1Init();
@@ -32,19 +66,18 @@ int main(void) {
     ms_delay(2);
     accelerometerInit();
     ms_delay(2);
-    accelerometerDetect();
+    //accelerometerDetect();
     
     gyroscopeInit();
-    gyroscopeDetect();
+    //gyroscopeDetect();
 
     while(1){
         ms_delay(1000);
+        printf("$");
+        printTime();
         printGyroscopeData();
-        ms_delay(2);
         printPressureAndTemp();
-        ms_delay(2);
         printAccelerometerData();
-        ms_delay(2);
         printGpsData();
         printf("\n");
     }
